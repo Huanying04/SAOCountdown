6193cf52cb4374894fe363d1ce08a8f8c223b43f
Huanying04 <ken911230@gmail.com>
2022-08-02 01:14:37 +0800

    fix time incorrect

--- const.js
+++ const.js
@@ -1,33 +1,33 @@
-const closed_beta = 1659279600000;
-const sale_begin = 1667142000000;
-const server_open_time = 1667707200000;
-const server_trap_time = 1667723400000;
-const fisrt_dead = 1667734200000;
-const coper_dead = 1667736000000;
-const tztt_d_f = 1669820400000;
-const seventyfive_floor_fight = 1730952000000;
-const seventyfive_floor_fight_end = 1730955600000;
-const sao_clear = 1730958900000;
-const sao_disconnect = 1730959200000;
+const closed_beta = 1659272400000;
+const sale_begin = 1667134800000;
+const server_open_time = 1667700000000;
+const server_trap_time = 1667716200000;
+const fisrt_dead = 1667727000000;
+const coper_dead = 1667728800000;
+const tztt_d_f = 1669813200000;
+const seventyfive_floor_fight = 1730944800000;
+const seventyfive_floor_fight_end = 1730948400000;
+const sao_clear = 1730951700000;
+const sao_disconnect = 1730952000000;
 
-const first_boss_fight = 1670125200000;
-const second_floor_open = 1670130000000; //小說沒明講，我隨便掰的
-const second_boss_defeated = 1670976000000; //小說沒明講，我隨便掰的
-const third_boss_defeated = 1671595920000;
-const forth_boss_defeated = 1672119120000;
-const fifth_boss_defeated = 1672486200000;
-const sixth_boss_defeated = 1672840800000;
+const first_boss_fight = 1670118000000;
+const second_floor_open = 1670122800000; //小說沒明講，我隨便掰的
+const second_boss_defeated = 1670968800000; //小說沒明講，我隨便掰的
+const third_boss_defeated = 1671588720000;
+const forth_boss_defeated = 1672111920000;
+const fifth_boss_defeated = 1672479000000;
+const sixth_boss_defeated = 1672833600000;
 
 //以下基本上都是估算的，剩至還有些東西被我省略了
 //結果我太懶懶得用這些資料
-const twentey_fifth_boss_defeated = 1680220800000;
-const twenty_eighth_boss_defeated = 1683590400000;
-const thirtieth_boss_defeated = 1687392000000;
-const thirty_eighth_boss_defeated = 1696723200000;
-const fourtieth_boss_defeated = 1697623200000;
-const fiftieth_boss_defeated = 1705190400000;
-const fifty_fifth_boss_defeated = 1708646400000;
-const starburst_stream_16_10 = 1729242000000;
+const twentey_fifth_boss_defeated = 1680213600000;
+const twenty_eighth_boss_defeated = 1683583200000;
+const thirtieth_boss_defeated = 1687384800000;
+const thirty_eighth_boss_defeated = 1696716000000;
+const fourtieth_boss_defeated = 1697616000000;
+const fiftieth_boss_defeated = 1705183200000;
+const fifty_fifth_boss_defeated = 1708639200000;
+const starburst_stream_16_10 = 1729234800000;
 
-const twenty_first_boss_defeated = 1684632208696; //用線性函數算出來的
-const twenty_second_boss_defeated = 1684891408696; //三天後
+const twenty_first_boss_defeated = 1684625008696; //用線性函數算出來的
+const twenty_second_boss_defeated = 1684884208696; // 三天後
